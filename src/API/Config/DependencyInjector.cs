87d8a6ebd5fa79b1089686df1083983fc1abe425
Matheus Silva <escorpion.b8@gmail.com>
2024-05-05 15:33:27 -0300

    initial commit

---
+++ src/API/Config/DependencyInjector.cs
@@ -0,0 +1,17 @@
+namespace API.Config;
+
+public static class DependencyInjector
+{
+    public static void RegisterDependecyInjector(this IServiceCollection services)
+    {
+
+    }
+    public static void RegisterDatabase(this IServiceCollection services)
+    {
+
+    }
+    public static void RegisterMenssageBus(this IServiceCollection services)
+    {
+
+    }
+}
