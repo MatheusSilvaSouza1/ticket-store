704c215f7d2f8c0925da29fdba586458d179361a
Matheus Silva <escorpion.b8@gmail.com>
2024-05-12 12:42:30 -0300

    Sera adicionado o volume na configuracao do docker compose

--- src/TicketStore/Infra/ContextFactory.cs
+++ src/TicketStore/Infra/ContextFactory.cs
@@ -8,7 +8,7 @@ public class ContextFactory : IDesignTimeDbContextFactory<Context>
     public Context CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<Context>();
-        optionsBuilder.UseNpgsql("Host=localhost:5432; Database=database; Username=ticket-store; Password=postgres");
+        optionsBuilder.UseNpgsql("Host=localhost; Database=ticket-store; Username=ticket-store; Password=postgres");
 
         return new Context(optionsBuilder.Options);
     }
